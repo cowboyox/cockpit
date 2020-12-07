cowboyox/cockpit

commit 38eb099f8e9455a65dbfd2e7d477aaf5f4495e19

tools: Split basic/optional package build on RHEL
On RHEL/CentOS, cockpit is delivered as two mostly identical source
packages.  "cockpit" with build_basic=1, and "cockpit-appstream" with
build_optional=1.

Do that split upstream, so that upstream testing better reflects what
actually happens downstream in RHEL. This will also finally exercise the
`build_*=0` code paths, so that we stop breaking them:

 - Check the OS and source package name in the spec to provide the
   correct values for build_*. This minimizes the downstream delta to
   just Name: and %changelog.

 - Adjust make-srpm to build a separate cockpit-appstream.spec on
   RHEL/CentOS, and build srpms for all generated specs.

Closes #14635

tools/cockpit.spec
@@ -39,11 +39,6 @@
 
 %define _hardened_build 1
 
-# build basic packages like cockpit-bridge
-%define build_basic 1
-# build optional extensions like cockpit-machines
-%define build_optional 1
-
 %define __lib lib
 
 %if 0%{?rhel}
@@ -69,6 +64,23 @@ Source0:        cockpit-%{version}.tar.gz
 %else
 Release:        1%{?dist}
 Source0:        https://github.com/cockpit-project/cockpit/releases/download/%{version}/cockpit-%{version}.tar.xz
+%endif
+
+# in RHEL the source package is duplicated: cockpit (building basic packages like cockpit-{bridge,system})
+# and cockpit-appstream (building optional packages like cockpit-{machines,pcp})
+%if 0%{?rhel}
+
+%if "%{name}" == "cockpit"
+%define build_basic 1
+%define build_optional 0
+%else
+%define build_basic 0
+%define build_optional 1
+%endif
+
+%else
+%define build_basic 1
+%define build_optional 1
 %endif
 
 BuildRequires: gcc

tools/make-srpm
@@ -53,15 +53,27 @@ sed -e "/^Version:.*/d" -e "1i\
 tmpdir=$(mktemp -d $PWD/srpm-build.XXXXXX)
 tar xaf "$source" -O cockpit-$version/tools/cockpit.spec | modify_spec > $tmpdir/cockpit.spec
 
-rpmbuild -bs \
-         --quiet \
-         --define "_sourcedir $(dirname $source)" \
-         --define "_specdir $tmpdir" \
-         --define "_builddir $tmpdir" \
-         --define "_srcrpmdir `pwd`" \
-         --define "_rpmdir $tmpdir" \
-         --define "_buildrootdir $tmpdir/.build" \
-         $tmpdir/cockpit.spec
+# on RHEL/CentOS, cockpit is delivered as two mostly identical source packages.
+# "cockpit" with build_basic=1, and "cockpit-appstream" with build_optional=1
+# the spec has proper build_* defaults depending on the Name:
+if grep -qE '^ID=.*(rhel|centos)' /etc/os-release; then
+    sed '/^Name:/ s/$/-appstream/' < $tmpdir/cockpit.spec > $tmpdir/cockpit-appstream.spec
+fi
 
-rpm --qf '%{Name}-%{Version}-%{Release}.src.rpm\n' -q --specfile $tmpdir/cockpit.spec | head -n1
+RPMNAMES=""
+
+for spec in $tmpdir/*.spec; do
+    rpmbuild -bs \
+             --quiet \
+             --define "_sourcedir $(dirname $source)" \
+             --define "_specdir $tmpdir" \
+             --define "_builddir $tmpdir" \
+             --define "_srcrpmdir `pwd`" \
+             --define "_rpmdir $tmpdir" \
+             --define "_buildrootdir $tmpdir/.build" \
+             $spec
+     RPMNAMES="$RPMNAMES $(rpm --qf '%{Name}-%{Version}-%{Release}.src.rpm\n' -q --specfile $spec | head -n1)"
+done
+
 rm -rf $tmpdir
+echo "$RPMNAMES"

commit ee3e293c1f4da07c922ef5f10ac8af768f46143a

tools: Build all binary packages for EPEL
Commit 38eb099f8 caused our COPR EPEL-8 builds to only build the basic
packages. This is not intended: Neither do COPR or EPEL repositories
have the BaseOS/AppStream split (so it doesn't make sense to split the
source package), nor do we ever release a cockpit-appstream.srpm to
them.

So make EPEL behave like Fedora and build all packages again.

Fixes #14974
Closes #14977

tools/cockpit.spec
@@ -68,7 +68,8 @@ Source0:        https://github.com/cockpit-project/cockpit/releases/download/%{v
 
 # in RHEL the source package is duplicated: cockpit (building basic packages like cockpit-{bridge,system})
 # and cockpit-appstream (building optional packages like cockpit-{machines,pcp})
-%if 0%{?rhel}
+# This split does not apply to EPEL/COPR.
+%if 0%{?rhel} && 0%{?epel} == 0
 
 %if "%{name}" == "cockpit"
 %define build_basic 1