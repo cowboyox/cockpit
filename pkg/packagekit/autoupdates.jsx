cowboyox/cockpit

commit d57e2c6ab073afe607f4a79cf3c5d11c6ece9890

packagekit: if dnf-automatic or dnf-automatic-install unit file parsing failed show an alert

pkg/packagekit/autoupdates.jsx
@@ -99,7 +99,7 @@ class DnfImpl extends ImplBase {
                             if (output.indexOf("InactiveSec=1d\n") >= 0)
                                 this.day = this.time = "";
                             else
-                                this.supported = false;
+                                this.parsing_failed = true;
                         }
 
                         debug(`dnf getConfig: supported ${this.supported}, enabled ${this.enabled}, type ${this.type}, day ${this.day}, time ${this.time}, installed ${this.installed}; raw response '${output}'`);
@@ -135,7 +135,7 @@ class DnfImpl extends ImplBase {
         if (words.length == 1 && validTime.test(words[0]))
             this.time = words[0].replace(/^0+/, "");
         else
-            this.supported = false;
+            this.parsing_failed = true;
     }
 
     setConfig(enabled, type, day, time) {
@@ -359,6 +359,11 @@ export class AutoUpdates extends React.Component {
 
         return (<>
             <div id="autoupdates-settings">
+                {enabled && this.state.backend.parsing_failed &&
+                <Alert isInline
+                       variant="info"
+                       className="autoupdates-card-error"
+                       title={_("Failed to parse unit files for dnf-automatic.timer or dnf-automatic-install.timer. Please remove custom overrides to enable this feature.")} />}
                 <Flex alignItems={{ default: 'alignItemsCenter' }}>
                     <Flex grow={{ default: 'grow' }} alignItems={{ default: 'alignItemsBaseline' }}>
                         <FlexItem>

test/verify/check-packagekit
@@ -1256,7 +1256,7 @@ class TestAutoUpdates(NoSubManCase):
             b.reload()
             b.enter_page("/updates")
             time.sleep(5)
-            b.wait_not_present("#autoupdates-settings")
+            b.wait_visible("#settings .pf-c-alert")
 
     def testWithAvailableUpdates(self):
         b = self.browser

pkg/packagekit/updates.scss
@@ -305,3 +305,7 @@ table.header-buttons {
 .cockpit-update-warning-text {
     color: var(--pf-global--warning-color--200);
 }
+
+.autoupdates-card-error {
+    margin-bottom: var(--pf-global--spacer--md);
+}

commit 4d8528da9313dd921f125bf7d03ff540703fcb74

packagekit: Don't show parse error if dnf-automatic is not installed
Commit d57e2c6ab07 caused the "Failed to parse timer" alert to show up
when dnf-automatic is not installed. The shell script's output in
DnfImpl.getConfig() is empty in that case. Only treat this as a parsing
error if the package is actually installed.

Revert that commit's partial renaming of `supported` to
`parsing_failed`. It was not done consistently, and `supported` is
already the flag for "system was customized so that the page can't
parse/edit it".

Fixes #15764

pkg/packagekit/autoupdates.jsx
@@ -44,7 +44,7 @@ function debug() {
 
 class ImplBase {
     constructor() {
-        this.supported = true; // false if system was customed in a way that we cannot parse
+        this.supported = true; // false if system was customized in a way that we cannot parse
         this.enabled = null; // boolean
         this.type = null; // "all" or "security"
         this.day = null; // systemd.time(7) day of week (e. g. "mon"), or empty for daily
@@ -98,8 +98,8 @@ class DnfImpl extends ImplBase {
                         } else {
                             if (output.indexOf("InactiveSec=1d\n") >= 0)
                                 this.day = this.time = "";
-                            else
-                                this.parsing_failed = true;
+                            else if (this.installed)
+                                this.supported = false;
                         }
 
                         debug(`dnf getConfig: supported ${this.supported}, enabled ${this.enabled}, type ${this.type}, day ${this.day}, time ${this.time}, installed ${this.installed}; raw response '${output}'`);
@@ -135,7 +135,7 @@ class DnfImpl extends ImplBase {
         if (words.length == 1 && validTime.test(words[0]))
             this.time = words[0].replace(/^0+/, "");
         else
-            this.parsing_failed = true;
+            this.supported = false;
     }
 
     setConfig(enabled, type, day, time) {
@@ -225,12 +225,7 @@ export function getBackend(forceReinit) {
                             backend = new DnfImpl();
                         // TODO: apt backend
                         if (backend)
-                            backend.getConfig().then(() => {
-                                if (!backend.installed)
-                                    resolve(backend);
-                                else
-                                    resolve(backend.supported ? backend : null);
-                            });
+                            backend.getConfig().then(() => resolve(backend));
                         else
                             resolve(null);
                     })
@@ -350,7 +345,7 @@ export class AutoUpdates extends React.Component {
 
         let desc = null;
 
-        if (this.state.backend.enabled) {
+        if (this.state.backend.enabled && this.state.backend.supported) {
             desc = this.state.backend.type == "security" ? _("Security updates ") : _("Updates ");
             desc += cockpit.format(_("will be applied $0 at $1"), days[this.state.backend.day], this.state.backend.time);
         }
@@ -359,7 +354,7 @@ export class AutoUpdates extends React.Component {
 
         return (<>
             <div id="autoupdates-settings">
-                {enabled && this.state.backend.parsing_failed &&
+                {enabled && this.state.backend.installed && !this.state.backend.supported &&
                 <Alert isInline
                        variant="info"
                        className="autoupdates-card-error"

test/verify/check-packagekit
@@ -1382,6 +1382,7 @@ class TestAutoUpdatesInstall(NoSubManCase):
 
         if self.backend == 'dnf':
             b.wait_in_text("#autoupdates-settings", "Not set up")
+            b.wait_not_present("#settings .pf-c-alert")
             b.click("#autoupdates-settings button:contains('Enable')")
         else:
             b.wait_not_present("#settings")
@@ -1421,6 +1422,7 @@ WantedBy=basic.target
 
         # click through install dialog
         b.wait_in_text("#autoupdates-settings", "Not set up")
+        b.wait_not_present("#settings .pf-c-alert")
         b.click("#autoupdates-settings button:contains('Enable')")
         b.wait_popup('dialog')
         b.wait_visible('#dialog button.apply')