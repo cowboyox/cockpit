cowboyox/cockpit

commit 017dcfd1e2a8d304f3d683378cc6af0a26370f21

systemd: Rewrite realmd configuration in React and standard PF4
The dialog is split into separate join and a leave components/modals
now, to make them easier to understand and maintain. For consistency
with other pages, split the `realms-op` dialog ID into
`realms-{join,leave}-dialog`.

Fix a few usability problems along the way, such as disabling the inputs
and Join button for missing/invalid inputs. Add checks for these.

Don't implement the `.Cancel()` API, as this was never wired up in the
UI -- the cancel button gets disabled as soon as the operation starts
(and the tests explicitly test that).

Move the realmd specific CSS out of overview.scss into
overview-cards/realmd.scss and remove obsolete bits.

test/verify/check-system-realms
@@ -102,27 +102,31 @@ class CommonTests:
         def set_address():
             # old realmd/IPA don't support realmd auto-detection yet
             if m.image.startswith("rhel-8") or m.image in ["centos-8-stream", "ubuntu-2004"]:
-                b.wait_attr("#realms-op-address", "data-discover", "empty")
+                b.wait_attr("#realms-op-address", "data-discover", "done")
                 b.wait_val(self.op_address, "")
-                b.set_val(self.op_address, "cockpit.lan")
+                b.wait_not_present("#realms-op-address-helper")
+                b.set_input_text(self.op_address, "cockpit.lan")
             else:
                 # on current OSes, domain and suggested admin get auto-detected
                 b.wait_val(self.op_address, "cockpit.lan")
 
         # Join cockpit.lan
         b.click(self.domain_sel)
-        b.wait_popup("realms-op")
+        b.wait_popup("realms-join-dialog")
         set_address()
-        b.wait_text(".realms-op-address-error", "Contacted domain")
-        # admin gets  auto-detected
+        b.wait_text("#realms-op-address-helper", "Contacted domain")
+        # admin gets auto-detected
         b.wait_val(self.op_admin, self.admin_user)
-        b.set_val(self.op_admin_password, self.admin_password)
-        b.wait_not_visible(".realms-op-leave-only-row")
-        b.click(".realms-op-apply")
+        b.set_input_text(self.op_admin_password, self.admin_password)
+        b.click(f"#realms-join-dialog button{self.primary_btn_class}")
         # running operation cannot be cancelled any more
-        b.wait_visible(".realms-op-cancel:disabled")
+        b.wait_visible("#realms-join-dialog button.pf-m-link:disabled")
+        # disables inputs during join
+        b.wait_visible("#realms-op-address:disabled")
+        b.wait_visible("#realms-op-admin:disabled")
+        b.wait_visible("#realms-op-admin-password:disabled")
         with b.wait_timeout(300):
-            b.wait_popdown("realms-op")
+            b.wait_not_present("#realms-join-dialog")
 
         # Check that this has worked
         wait_number_domains(1)
@@ -172,20 +176,19 @@ class CommonTests:
         b.login_and_go("/system")
         b.wait_in_text(self.domain_sel, "cockpit.lan")
         b.click(self.domain_sel)
-        b.wait_popup("realms-op")
+        b.wait_popup("realms-leave-dialog")
         b.wait_text("#realms-op-info-domain", "cockpit.lan")
         b.wait_text("#realms-op-info-login-format", "username@cockpit.lan")
         b.wait_text("#realms-op-info-server-sw", self.expected_server_software)
         b.wait_text("#realms-op-info-client-sw", "sssd")
         # leave button should be hidden behind expander by default
-        b.wait_not_visible("button.realms-op-leave")
-        b.wait_not_visible("#realms-op-alert")
-        b.click("#realms-op-leave-toggle")
-        b.wait_visible("#realms-op-alert")
-        b.wait_visible("button.realms-op-leave")
-        b.click(".realms-op-leave")
+        b.wait_not_visible("#realms-op-leave")
+        b.wait_not_visible("#realms-leave-dialog .pf-c-alert")
+        b.click("#realms-leave-dialog .pf-c-expandable-section__toggle")
+        b.wait_visible("#realms-leave-dialog .pf-c-alert")
+        b.click("#realms-op-leave")
 
-        b.wait_popdown("realms-op")
+        b.wait_not_present("#realms-leave-dialog")
         wait_number_domains(0)
         # re-enables hostname changing
         b.wait_visible("#system_information_hostname_button:not([disabled])")
@@ -201,34 +204,32 @@ class CommonTests:
         while tries < 3:
             # Send a wrong password
             b.click(self.domain_sel)
-            b.wait_popup("realms-op")
+            b.wait_popup("realms-join-dialog")
             set_address()
             b.wait_val(self.op_admin, self.admin_user)
-            b.set_val(self.op_admin_password, "foo")
-            b.click(".realms-op-apply")
-            b.wait_text_not(".realms-op-message", "")
-            error = b.text(".realms-op-message")
-            b.wait_not_visible(".realms-op-leave-only-row")
+            b.set_input_text(self.op_admin_password, "foo")
+            b.click(f"#realms-join-dialog button{self.primary_btn_class}")
+            b.wait_text_not(".realms-op-error", "")
+            error = b.text(".realms-op-error")
             if "Already running another action" not in error:
                 # "More" link is part of the message component, so this looks a little funny here
                 if self.expected_server_software == 'active-directory':
-                    self.assertEqual(error, "Failed to join the domain More")
+                    self.assertEqual(error, "Danger alert:Failed to join the domainDetails")
                 else:
-                    self.assertEqual(error, "Password is incorrect More")
+                    self.assertEqual(error, "Danger alert:Password is incorrectDetails")
                 # "More" should be visible, and diagnostics not shown by default
-                b.wait_not_visible(".realms-op-diagnostics")
-                b.click(".realms-op-more-diagnostics")
-                # that hides the initial message and the More link
-                b.wait_not_visible(".realms-op-message")
-                b.wait_not_visible(".realms-op-more-diagnostics")
+                b.wait_not_present(".realms-op-diagnostics")
+                b.click(".realms-op-error button")
+                # that hides the Details link
+                b.wait_not_present(".realms-op-error button")
                 # and shows the raw log
                 b.wait_visible(".realms-op-diagnostics")
                 if self.expected_server_software == 'active-directory':
                     b.wait_in_text(".realms-op-diagnostics", "Couldn't authenticate as: Administrator@COCKPIT.LAN")
                 else:
                     b.wait_in_text(".realms-op-diagnostics", "ipa-client-install command failed")
-            b.click(".realms-op-cancel")
-            b.wait_popdown("realms-op")
+            b.click("#realms-join-dialog button.pf-m-link")
+            b.wait_not_present("#realms-join-dialog")
             if "Already running another action" not in error:
                 break
             print("Another operation running, retry")
@@ -237,14 +238,14 @@ class CommonTests:
 
         # Try to join a non-existing domain
         b.click(self.domain_sel)
-        b.wait_popup("realms-op")
-        b.set_val(self.op_address, "NOPE")
-        b.wait_text(".realms-op-address-error", "Domain NOPE could not be contacted")
-        b.wait_not_visible(".realms-op-address-spinner")
-        b.click(".realms-op-apply")
-        b.wait_not_visible(".realms-op-address-spinner")
-        b.click(".realms-op-cancel")
-        b.wait_popdown("realms-op")
+        b.wait_popup("realms-join-dialog")
+        # wait for auto-detection
+        set_address()
+        b.set_input_text(self.op_address, "NOPE")
+        b.wait_text("#realms-op-address-helper", "Domain could not be contacted")
+        b.wait_visible(f"#realms-join-dialog button{self.primary_btn_class}:disabled")
+        b.click("#realms-join-dialog button.pf-m-link")
+        b.wait_not_present("#realms-join-dialog")
 
         self.allow_journal_messages(".*No authentication agent found.*")
         self.allow_restart_journal_messages()
@@ -274,13 +275,13 @@ class CommonTests:
 
         # Show domain information
         b.click(self.domain_sel)
-        b.wait_popup("realms-op")
+        b.wait_popup("realms-leave-dialog")
         b.wait_text("#realms-op-info-domain", "cockpit.lan")
         b.wait_text("#realms-op-info-login-format", "username")  # no @domain
         b.wait_text("#realms-op-info-server-sw", self.expected_server_software)
         b.wait_text("#realms-op-info-client-sw", "sssd")
-        b.click(".realms-op-cancel")
-        b.wait_popdown("realms-op")
+        b.click(f"#realms-leave-dialog button{self.default_btn_class}")
+        b.wait_not_present("#realms-leave-dialog")
 
         # should be able to run admin operations
         b.go('/system/services#/systemd-tmpfiles-clean.timer')
@@ -324,14 +325,14 @@ class CommonTests:
         # join client machine with Cockpit, to create the HTTP/ principal and /etc/cockpit/krb5.keytab
         self.login_and_go("/system")
         b.click("#system_information_domain_button")
-        b.wait_popup("realms-op")
-        b.set_val("#realms-op-address", "cockpit.lan")
-        b.wait_text(".realms-op-address-error", "Contacted domain")
-        b.set_val("#realms-op-admin", self.admin_user)
-        b.set_val("#realms-op-admin-password", self.admin_password)
-        b.click(".realms-op-apply")
+        b.wait_popup("realms-join-dialog")
+        b.set_input_text("#realms-op-address", "cockpit.lan")
+        b.wait_text("#realms-op-address-helper", "Contacted domain")
+        b.set_input_text("#realms-op-admin", self.admin_user)
+        b.set_input_text("#realms-op-admin-password", self.admin_password)
+        b.click(f"#realms-join-dialog button{self.primary_btn_class}")
         with b.wait_timeout(300):
-            b.wait_popdown("realms-op")
+            b.wait_not_present("#realms-join-dialog")
         b.logout()
         m.execute('while ! id alice; do sleep 5; systemctl restart sssd; done', timeout=300)
 
@@ -526,7 +527,7 @@ ExecStart=/bin/true
         b.click("#hosts-sel button")
         b.click("button:contains('Add new host')")
         b.wait_popup('hosts_setup_server_dialog')
-        b.set_val('#add-machine-address', "x0.cockpit.lan")
+        b.set_input_text('#add-machine-address', "x0.cockpit.lan")
         b.click('#hosts_setup_server_dialog .modal-footer button:contains(Add)')
 
         # with --pubkey support this should Just Work, otherwise confirm fingerprint
@@ -614,15 +615,16 @@ ExecStart=/bin/true
 
         # Join cockpit.lan
         b.click(self.domain_sel)
-        b.wait_popup("realms-op")
-        b.set_val(self.op_address, "cockpit.lan")
-        b.wait_in_text(".realms-op-address-error", "Domain cockpit.lan is not supported")
+        b.wait_popup("realms-join-dialog")
+        b.wait_attr("#realms-op-address", "data-discover", "done")
+        b.set_input_text(self.op_address, "cockpit.lan")
+        b.wait_in_text("#realms-op-address-helper", "Domain is not supported")
         # no admin name auto-detection for unsupported domains
         b.wait_val(self.op_admin, "")
-        b.set_val(self.op_admin, self.admin_user)
-        b.set_val(self.op_admin_password, self.admin_password)
-        b.click(".realms-op-apply")
-        b.wait_text(".realms-op-message", "Joining this domain is not supported")
+        b.set_input_text(self.op_admin, self.admin_user)
+        b.set_input_text(self.op_admin_password, self.admin_password)
+        # Join button disabled
+        b.wait_visible(f"#realms-join-dialog button{self.primary_btn_class}:disabled")
 
         self.allow_journal_messages(".*couldn't introspect /org/freedesktop/realmd.*",
                                     "sudo: unable to resolve host x0.cockpit.lan: Name or service not known")
@@ -1145,17 +1147,16 @@ class TestPackageInstall(packagelib.PackageCase):
         b.wait_not_present(".pf-c-modal-box:contains('Install software')")
 
         # should continue straight to join dialog
-        b.wait_visible("#realms-op")
-        b.wait_popup("realms-op")
+        b.wait_visible("#realms-join-dialog")
 
         # no auto-detected domain/admin
-        b.wait_attr("#realms-op-address", "data-discover", "empty")
+        b.wait_attr("#realms-op-address", "data-discover", "done")
         self.assertEqual(b.val("#realms-op-address"), "")
         self.assertEqual(b.val("#realms-op-admin"), "")
 
         # no running IPA server for this test, so just cancel
-        b.click(".realms-op-cancel")
-        b.wait_popdown("realms-op")
+        b.click("#realms-join-dialog button.pf-m-link")
+        b.wait_not_present("#realms-join-dialog")
 
         # should not have a tooltip any more
         b.wait_not_present("#system_information_domain_tooltip")

pkg/systemd/overview-cards/configurationCard.jsx
@@ -24,11 +24,10 @@ import {
 
 import host_keys_script from "raw-loader!./ssh-list-host-keys.sh";
 import cockpit from "cockpit";
-import { EmptyStatePanel } from "cockpit-components-empty-state.jsx";
-import { Privileged } from "cockpit-components-privileged.jsx";
-import { ServerTimeConfig } from 'serverTime.js';
-import * as realmd from "./realmd-operation.js";
 import { superuser } from "superuser";
+import { EmptyStatePanel } from "cockpit-components-empty-state.jsx";
+import { ServerTimeConfig } from 'serverTime.js';
+import { RealmdClient, RealmButton } from "./realmd.jsx";
 
 import "./configurationCard.scss";
 
@@ -43,36 +42,23 @@ export class ConfigurationCard extends React.Component {
             showKeysModal: false,
         };
 
-        this.realmd = realmd.setup();
+        this.realmd_client = new RealmdClient();
+        superuser.addEventListener("changed", () => this.realmd_client.initProxy());
     }
 
     componentDidMount() {
-        this.realmd.addEventListener("changed", () => this.setState({}));
+        this.realmd_client.addEventListener("changed", () => this.setState({}));
     }
 
     render() {
         let hostname_button = null;
-        if (superuser.allowed && !this.realmd.hostname_button_disabled)
+        if (superuser.allowed && this.realmd_client.allowHostnameChange())
             hostname_button = (
                 <Button id="system_information_hostname_button" variant="link"
                         onClick={ () => this.setState({ hostEditModal: true }) }
                         isInline aria-label="edit hostname">
                     {this.props.hostname !== "" ? _("edit") : _("Set hostname")}
                 </Button>);
-
-        const domain_tooltip = superuser.allowed && this.realmd.button_tooltip;
-        const domain_disabled = !superuser.allowed || this.realmd.button_disabled;
-
-        const domain_button = (
-            <Privileged allowed={ !domain_tooltip }
-                        tooltipId="system_information_domain_tooltip"
-                        excuse={ domain_tooltip }>
-                <Button id="system_information_domain_button" variant="link"
-                        onClick={ () => this.realmd.clicked() }
-                        isInline isDisabled={ domain_disabled } aria-label="join domain">
-                    { superuser.allowed ? this.realmd.button_text : _("Not joined") }
-                </Button>
-            </Privileged>);
 
         return (
             <>
@@ -98,7 +84,7 @@ export class ConfigurationCard extends React.Component {
 
                                 <tr>
                                     <th scope="row">{_("Domain")}</th>
-                                    <td>{domain_button}</td>
+                                    <td><RealmButton realmd_client={this.realmd_client} /></td>
                                 </tr>
 
                                 <tr>

pkg/systemd/overview-cards/realmd-operation.html
@@ -1,96 +1,0 @@
-<div hidden aria-hidden="true">
-    <div class="pf-c-backdrop">
-        <div class="pf-l-bullseye">
-            <div class="pf-c-modal-box pf-m-md pf-m-align-top" role="dialog" aria-modal="true">
-                <header class="pf-c-modal-box__header">
-                    <h1 class="pf-c-modal-box__title realms-op-title"></h4>
-                </header>
-                <div class="pf-c-modal-box__body">
-                    <div class="realms-op-fields">
-                      <div class="realms-op-leave-only-row">
-                        <form class="ct-form">
-                          <label class="control-label" for="realms-op-info-domain" translate="yes">Domain</label>
-                          <span id="realms-op-info-domain"></span>
-
-                          <label class="control-label" for="realms-op-info-login-format" translate="yes">Login format</label>
-                          <span id="realms-op-info-login-format"></span>
-
-                          <label class="control-label" for="realms-op-info-server-sw" translate="yes">Server software</label>
-                          <span id="realms-op-info-server-sw"></span>
-
-                          <label class="control-label" for="realms-op-info-client-sw" translate="yes">Client software</label>
-                          <span id="realms-op-info-client-sw"></span>
-                        </form>
-
-                        <a href="#" id="realms-op-leave-toggle" translate="yes">Leave domain</a>
-                        <span id="realms-op-leave-caret" class="fa fa-caret-right"></span>
-                        <div id="realms-op-alert" class="alert alert-warning" hidden>
-                          <div translate="yes">After leaving the domain, only users with local credentials will be able to log into this machine. This may also affect other services as DNS resolution settings and the list of trusted CAs may change.</div>
-                          <button class="pf-c-button pf-m-danger realms-op-leave" type="submit" translate="yes">Leave domain</button>
-                        </div>
-                      </div>
-
-                      <form class="ct-form realms-op-join-only">
-                        <label class="control-label" for="realms-op-address" translate="yes">Domain address</label>
-
-                        <div class="ct-validation-wrapper">
-                          <input id="realms-op-address" class="realms-op-field form-control" type="text" placeholder="domain.example.com" value="" />
-
-                          <div class="help-block">
-                            <span class="realms-op-address-spinner" hidden></span>
-                            <span class="realms-op-address-error"></span>
-                          </div>
-                        </div>
-
-                        <label class="control-label" for="realms-op-auth" translate="yes">Authentication</label>
-                        <div id="realms-op-auth" class="btn-group bootstrap-select dropdown form-control">
-                          <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-                              <span class="pull-left"></span>
-                              <i class="fa fa-caret-down pf-c-context-selector__toggle-icon" aria-hidden="true"></i>
-                          </button>
-                          <ul class="dropdown-menu" />
-                        </div>
-
-                        <label class="control-label" for="realms-op-user" translate="yes">User name</label>
-                        <input id="realms-op-user" class="realms-op-field form-control" type="text" value=""/>
-
-                        <label class="control-label" for="realms-op-user-password" translate="yes">User password</label>
-                        <input id="realms-op-user-password" class="realms-op-field form-control" type="password" value=""/>
-
-                        <label class="control-label" for="realms-op-admin" translate="yes">Domain administrator name</label>
-                        <input id="realms-op-admin" class="realms-op-field form-control" type="text" placeholder="admin" value=""/>
-
-                        <label class="control-label" for="realms-op-admin-password" translate="yes">Domain administrator password</label>
-                        <input id="realms-op-admin-password" class="realms-op-field form-control" type="password" value=""/>
-
-                        <label class="control-label" for="realms-op-ot-password" translate="yes">One time password</label>
-                        <input id="realms-op-ot-password" class="realms-op-field form-control" type="password" value=""/>
-                      </form>
-                      <pre class="realms-op-diagnostics" hidden></pre>
-                    </div>
-                </div>
-                <footer class="pf-c-modal-box__footer">
-                    <div class="modal-footer">
-                        <div class="pf-c-alert pf-m-danger pf-m-inline realms-op-error" aria-label="inline danger alert">
-                            <div class="pf-c-alert__icon">
-                            <span class="fa fa-exclamation-circle"></span>
-                        </div>
-                        <h4 class="pf-c-alert__title realms-op-message"></h4>
-                    </div>
-                    <button class="pf-c-button pf-m-primary pf-m-progress pf-m-in-progress realms-op-apply">
-                        <span class="realms-op-spinner pf-c-button__progress">
-                            <span class="pf-c-spinner pf-m-md" role="progressbar" aria-valuetext="Loading...">
-                                <span class="pf-c-spinner__clipper"></span>
-                                <span class="pf-c-spinner__lead-ball"></span>
-                                <span class="pf-c-spinner__tail-ball"></span>
-                            </span>
-                        </span>
-                        <span translate="yes">Join</span>
-                    </button>
-                    <button class="pf-c-button pf-m-link realms-op-cancel" translate="yes">Cancel</button>
-                    <span class="realms-op-wait-message" translate="yes" hidden>This may take a while</span>
-                </footer>
-            </div>
-        </div>
-    </div>
-</div>

pkg/systemd/overview-cards/realmd.scss
@@ -1,0 +1,13 @@
+.realms-op-diagnostics {
+  max-height: 200px;
+}
+
+.realms-op-wait-message {
+  margin-left: 10px;
+  float: right;
+  margin-top: 3px;
+}
+
+#realms-leave-dialog .pf-c-expandable-section {
+  margin-top: var(--pf-global--spacer--md);
+}

pkg/systemd/overview.scss
@@ -250,38 +250,6 @@
   margin-left: 1rem;
 }
 
-.realms-op-diagnostics {
-  /* standard PF alerts have a wide margin */
-  max-width: 550px;
-  text-align: left;
-  max-height: 200px;
-}
-
-.realms-op-error .realms-op-more-diagnostics {
-  font-weight: normal;
-}
-
-.realms-op-leave-only-row .pf-c-alert {
-  /* standard PF alerts have a wide margin */
-  padding-left: 2ex;
-
-  button {
-    margin: 1ex 0;
-  }
-}
-
-#realms-op-leave-toggle {
-  font-weight: bold;
-  /* leave some space between form and leave toggle */
-  line-height: 5rem;
-}
-
-.realms-op-wait-message {
-  margin-left: 10px;
-  float: right;
-  margin-top: 3px;
-}
-
 /* Add a subtle dropshadow to the alerts, to separate them from the background, similar to the cards on the page */
 .pf-c-page__main-section:not(.ct-overview-header),
 .pf-l-gallery {

test/verify/check-system-s4u
@@ -58,14 +58,15 @@ class TestS4USsh(MachineCase):
         # join client machine with Cockpit, to create the HTTP/ principal
         self.login_and_go("/system")
         b.click("#system_information_domain_button")
-        b.wait_popup("realms-op")
-        b.set_val("#realms-op-address", "cockpit.lan")
-        b.wait_text(".realms-op-address-error", "Contacted domain")
-        b.set_val("#realms-op-admin", "admin")
-        b.set_val("#realms-op-admin-password", "foobarfoo")
-        b.click(".realms-op-apply")
+        b.wait_popup("realms-join-dialog")
+        b.wait_attr("#realms-op-address", "data-discover", "done")
+        b.set_input_text("#realms-op-address", "cockpit.lan")
+        b.wait_text("#realms-op-address-helper", "Contacted domain")
+        b.set_input_text("#realms-op-admin", "admin")
+        b.set_input_text("#realms-op-admin-password", "foobarfoo")
+        b.click(f"#realms-join-dialog button{self.primary_btn_class}")
         with b.wait_timeout(300):
-            b.wait_popdown("realms-op")
+            b.wait_not_present("#realms-join-dialog")
         b.logout()
         # the above installed a new certificate; pick this up, enable cert auth and TLS
         client_machine.write("/etc/cockpit/cockpit.conf", '[WebService]\nClientCertAuthentication = true\n', append=True)

commit 864acce826644432233ba5f891c30ea498b177ed

systemd: Fix realmd join crash if given address is not the domain name
It is actually possible and legit for the "Address:" input to be the DNS
name of the IdM server instead of the actual domain name. realmd's
Discover API is fine with that, and just returns the actual domain name
in `reply.realm.Name`.

Commit 017dcfd1e2 introduced an additional assertion that the queried
and returned domain names are the same, but this does not hold with the
above. Drop the assertion, and add an integration test which covers this
scenario.

Fixes #16477

pkg/systemd/overview-cards/realmd.jsx
@@ -343,8 +343,7 @@ const JoinDialog = ({ realmd_client, onClose }) => {
                         if (name == "") {
                             if (!address)
                                 setAddress(reply.realm.Name);
-                        } else if (name != reply.realm.Name)
-                            throw new Error("checkRealm returned domain", reply.realm.Name, "for query", name);
+                        }
 
                         if (reply.kerberos_membership && reply.kerberos_membership.valid) {
                             setAddressValid("success");

test/verify/check-system-realms
@@ -250,6 +250,20 @@ class CommonTests:
         b.click("#realms-join-dialog button.pf-m-link")
         b.wait_not_present("#realms-join-dialog")
 
+        # Join a domain with the server as address (input differs from domain name)
+        b.click(self.domain_sel)
+        b.wait_popup("realms-join-dialog")
+        b.wait_attr("#realms-op-address", "data-discover", "done")
+        b.set_input_text(self.op_address, "f0.cockpit.lan")
+        b.wait_text("#realms-op-address-helper", "Contacted domain")
+        # admin gets auto-detected
+        b.wait_val(self.op_admin, self.admin_user)
+        b.set_input_text(self.op_admin_password, self.admin_password)
+        b.click(f"#realms-join-dialog button{self.primary_btn_class}")
+        with b.wait_timeout(300):
+            b.wait_not_present("#realms-join-dialog")
+        wait_number_domains(1)
+
         self.allow_journal_messages(".*No authentication agent found.*")
         self.allow_restart_journal_messages()
         # sometimes polling for info and joining a domain creates this noise