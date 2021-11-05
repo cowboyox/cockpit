cowboyox/cockpit

commit f015dccdf4a76105d01cd8e755890e79281b6502

networkmanager: confirm breaking change popup to React

pkg/networkmanager/interfaces.js
@@ -55,6 +55,28 @@ export function show_unexpected_error(error) {
     show_error_dialog(_("Unexpected error"), error.message || error);
 }
 
+function show_breaking_change_dialog({ fail_text, anyway_text, action }) {
+    const props = {
+        titleIconVariant: "warning",
+        id: "confirm-breaking-change-popup",
+        title: _("Connection will be lost"),
+        body: <p>{fail_text}</p>
+    };
+
+    const footer = {
+        actions: [
+            {
+                caption: anyway_text,
+                clicked: action,
+                style: "danger",
+            }
+        ],
+        cancel_caption: _("Keep connection")
+    };
+
+    show_modal_dialog(props, footer);
+}
+
 export function connection_settings(c) {
     if (c && c.Settings && c.Settings.connection) {
         return c.Settings.connection;
@@ -1508,7 +1530,6 @@ export function with_checkpoint(model, modify, options) {
     const curtain = $('#testing-connection-curtain');
     const curtain_testing = $('#testing-connection-curtain-testing');
     const curtain_restoring = $('#testing-connection-curtain-restoring');
-    const dialog = $('#confirm-breaking-change-popup');
 
     let curtain_timeout;
     let curtain_title_timeout;
@@ -1569,15 +1590,14 @@ export function with_checkpoint(model, modify, options) {
                                 manager.checkpoint_destroy(cp)
                                         .always(hide_curtain)
                                         .fail(function () {
-                                            dialog.find('#confirm-breaking-change-text').html(options.fail_text);
-                                            dialog.find('.pf-c-modal-box__footer button.pf-m-danger')
-                                                    .off('click')
-                                                    .text(options.anyway_text)
-                                                    .syn_click(model, function () {
-                                                        dialog.prop('hidden', true);
+                                            show_breaking_change_dialog({
+                                                ...options,
+                                                action: () => {
+                                                    syn_click(model, function () {
                                                         modify();
                                                     });
-                                            dialog.prop('hidden', false);
+                                                }
+                                            });
                                         });
                             }, settle_time * 1000);
                         })
@@ -1797,7 +1817,4 @@ export function apply_group_member(choices, model, apply_group, group_connection
 
 export function init() {
     cockpit.translate();
-
-    $('#confirm-breaking-change-popup [data-dismiss]').click(() =>
-        $('#confirm-breaking-change-popup').prop('hidden', true));
 }

pkg/lib/cockpit-components-dialog.jsx
@@ -207,6 +207,8 @@ DialogFooter.propTypes = {
  *      action is triggered.
  *  - footer (react element, top element should be of class modal-footer)
  *  - id optional, id that is assigned to the top level dialog node, but not the backdrop
+ *  - variant: See PF4 Modal component's 'variant' property
+ *  - titleIconVariant: See PF4 Modal component's 'titleIconVariant' property
  */
 export class Dialog extends React.Component {
     componentDidMount() {
@@ -233,6 +235,7 @@ export class Dialog extends React.Component {
 
         return (
             <Modal position="top" variant={this.props.variant || "medium"}
+                   titleIconVariant={this.props.titleIconVariant}
                    onEscapePress={() => undefined}
                    showClose={false}
                    id={this.props.id}

pkg/networkmanager/index.html
@@ -40,25 +40,5 @@
     <h1 id="testing-connection-curtain-restoring" translate="yes">Restoring connection</h1>
   </div>
 
-  <div hidden aria-hidden="true" id="confirm-breaking-change-popup">
-    <div class="pf-c-backdrop">
-      <div class="pf-l-bullseye">
-        <div class="pf-c-modal-box pf-m-md pf-m-align-top" role="dialog" aria-modal="true">
-          <header class="pf-c-modal-box__header">
-            <h1 class="pf-c-modal-box__title" translate="yes">Connection will be lost</h1>
-          </header>
-          <div class="pf-c-modal-box__body">
-            <div class="pficon pficon-warning-triangle-o"></div>
-            <div id="confirm-breaking-change-text"></div>
-          </div>
-          <footer class="pf-c-modal-box__footer">
-            <button class="pf-c-button pf-m-danger"></button>
-            <button class="pf-c-button pf-m-primary" data-dismiss="modal" translate>Keep connection</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-  </div>
-
 </body>
 </html>

pkg/networkmanager/networking.scss
@@ -143,13 +143,6 @@ th {
     z-index: 2000;
 }
 
-#confirm-breaking-change-popup .pficon-warning-triangle-o {
-    float: left;
-    font-size: 36px;
-    margin-top: 6px;
-    margin-right: 20px;
-}
-
 .pf-l-flex > .network-ip-settings-method {
     width: 12rem;
 }

test/verify/check-networkmanager-checkpoints
@@ -49,7 +49,7 @@ class TestNetworkingCheckpoints(NetworkCase):
 
         # Wait for dialog to appear and dismiss it
         b.click("#confirm-breaking-change-popup button:contains('Keep connection')")
-        b.wait_not_visible("#confirm-breaking-change-popup")
+        b.wait_not_present("#confirm-breaking-change-popup")
 
         if m.image not in ["debian-testing", "debian-stable"]:
             # Change IP
@@ -60,7 +60,7 @@ class TestNetworkingCheckpoints(NetworkCase):
             b.set_input_text('#network-ip-settings-netmask-0', "24")
             b.click("#network-ip-settings-apply")
             b.click("#confirm-breaking-change-popup button:contains('Keep connection')")
-            b.wait_not_visible("#confirm-breaking-change-popup")
+            b.wait_not_present("#confirm-breaking-change-popup")
 
     @skipImage("Main interface settings are read-only", "debian-stable", "debian-testing")
     @skipImage("not using dhclient", "fedora-coreos")
@@ -131,7 +131,7 @@ class TestNetworkingCheckpoints(NetworkCase):
         self.toggle_onoff(".pf-c-card__header:contains('%s')" % iface)
         with b.wait_timeout(120):
             b.click("#confirm-breaking-change-popup button:contains('Keep connection')")
-            b.wait_not_visible("#confirm-breaking-change-popup")
+            b.wait_not_present("#confirm-breaking-change-popup")
 
 
 if __name__ == '__main__':

commit 6cbebaacc03618efe80d2e239db48dc061ce34cc

networkmanager: Fix JavaScript error when applying settings
show_modal_dialog expects an action to be a function which returns a
promise, as it didn't you would get an error that undefined has no
method 'then'. The issue was introduced in f015dccdf4a76.

pkg/networkmanager/interfaces.js
@@ -1268,7 +1268,7 @@ export function syn_click(model, fun) {
     return function() {
         const self = this;
         const self_args = arguments;
-        model.synchronize().then(function() {
+        return model.synchronize().then(function() {
             fun.apply(self, self_args);
         });
     };
@@ -1556,11 +1556,7 @@ export function with_checkpoint(model, modify, options) {
                                         .catch(function () {
                                             show_breaking_change_dialog({
                                                 ...options,
-                                                action: () => {
-                                                    syn_click(model, function () {
-                                                        modify();
-                                                    });
-                                                }
+                                                action: syn_click(model, modify)
                                             });
                                         })
                                         .finally(hide_curtain);