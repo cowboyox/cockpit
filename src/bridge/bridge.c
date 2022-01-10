cowboyox/cockpit

commit 47bc8b93b7604b0831090e17e043286c4a81f1cb

common: Clean up fdwalk duplication, move to Linux close_range API
Linux 5.9 finally added a close_range() call [1]. glibc wraps this [2],
but not in a stable release yet. Let's move our code to be ready to use
it once it is available. Add a new common cockpit_close_range() function
which offers the same API, and use it everywhere.

Remove cockpit_unix_fd_close_*() from cockpitunixfd.h. It does not fit
there, as (1) that file otherwise contains backports of [2] and will
eventually disappear, and (2) we also want to use close_range() from
programs that don't include glib.

Link the new cockpitcloserange.c to pam-ssh-add and cockpit-session, to
drop their duplicated fdwalk()/closefd() implementations.

[1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=278a5fbaed89dacd04e9d
[2] https://sourceware.org/git/?p=glibc.git;a=commit;h=286286283e9bdc
[3] https://developer.gnome.org/glib/stable/glib-UNIX-specific-utilities-and-integration.html

src/bridge/bridge.c
@@ -39,13 +39,13 @@
 
 #include "common/cockpitassets.h"
 #include "common/cockpitchannel.h"
+#include "common/cockpitcloserange.h"
 #include "common/cockpitfdpassing.h"
 #include "common/cockpithacks-glib.h"
 #include "common/cockpitjson.h"
 #include "common/cockpitpipetransport.h"
 #include "common/cockpitsystem.h"
 #include "common/cockpittest.h"
-#include "common/cockpitunixfd.h"
 #include "common/cockpitwebresponse.h"
 
 #include <sys/prctl.h>
@@ -216,11 +216,11 @@ start_dbus_daemon (void)
 }
 
 static void
-setup_ssh_agent (gpointer addrfd)
+setup_ssh_agent (gpointer data)
 {
   g_unsetenv ("G_DEBUG");
   prctl (PR_SET_PDEATHSIG, SIGTERM);
-  cockpit_unix_fd_close_all (3, GPOINTER_TO_INT (addrfd));
+  cockpit_close_range (3, INT_MAX, 0);
 }
 
 static GPid
@@ -257,7 +257,7 @@ start_ssh_agent (void)
 
   if (!g_spawn_sync (NULL, agent_argv, NULL,
                      G_SPAWN_SEARCH_PATH, setup_ssh_agent,
-                     GINT_TO_POINTER (-1),
+                     NULL,
                      &agent_output, &agent_error,
                      &status, &error))
     {

src/common/Makefile-common.am
@@ -56,6 +56,8 @@ noinst_LIBRARIES += libcockpit-common.a
 libcockpit_common_a_SOURCES = \
 	src/common/cockpitchannel.c \
 	src/common/cockpitchannel.h \
+	src/common/cockpitcloserange.c \
+	src/common/cockpitcloserange.h \
 	src/common/cockpitcontrolmessages.c \
 	src/common/cockpitcontrolmessages.h \
 	src/common/cockpiterror.h src/common/cockpiterror.c \

src/common/cockpitcloserange.h
@@ -1,0 +1,5 @@
+#pragma once
+
+/* Added in Linux 5.9: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=278a5fbaed89dacd04e9d
+ * will eventually be in glibc: https://sourceware.org/git/?p=glibc.git;a=commit;h=286286283e9bdc */
+int cockpit_close_range (int from, int max_fd, int flags);

src/common/cockpitpipe.c
@@ -21,6 +21,7 @@
 
 #include "cockpitpipe.h"
 
+#include "cockpitcloserange.h"
 #include "cockpitflow.h"
 #include "cockpitunixfd.h"
 #include "cockpitunicode.h"
@@ -1407,7 +1408,7 @@ cockpit_pipe_pty (const gchar **argv,
   pid = forkpty (&fd, NULL, NULL, &winsz);
   if (pid == 0)
     {
-      if (cockpit_unix_fd_close_all (3, -1) < 0)
+      if (cockpit_close_range (3, INT_MAX, 0) < 0)
         {
           g_printerr ("couldn't close file descriptors\n");
           _exit (127);

src/pam-ssh-add/Makefile.am
@@ -1,6 +1,8 @@
 noinst_LIBRARIES += libpam_ssh_add.a
 
 libpam_ssh_add_a_SOURCES = \
+	src/common/cockpitcloserange.h \
+	src/common/cockpitcloserange.c \
 	src/pam-ssh-add/pam-ssh-add.c \
 	src/pam-ssh-add/pam-ssh-add.h \
 	$(NULL)

src/pam-ssh-add/pam-ssh-add.c
@@ -28,11 +28,10 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/resource.h>
-#include <dirent.h>
 #include <signal.h>
 #include <assert.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -46,6 +45,8 @@
 #include <security/pam_modules.h>
 
 #include "pam-ssh-add.h"
+
+#include "../common/cockpitcloserange.h"
 
 /* programs that can be overwidden in tests */
 const char *pam_ssh_agent_program = PATH_SSH_AGENT;
@@ -189,92 +190,6 @@ foreach_line (char *lines,
   return ret;
 }
 
-static int
-closefd (void *data,
-         int fd)
-{
-  int *from = data;
-  if (fd >= *from)
-    {
-      while (close (fd) < 0)
-        {
-          if (errno == EAGAIN || errno == EINTR)
-            continue;
-          if (errno == EBADF || errno == EINVAL)
-            break;
-          message ("couldn't close fd in child process: %m");
-          return -1;
-        }
-    }
-
-  return 0;
-}
-
-#ifndef HAVE_FDWALK
-
-static int
-fdwalk (int (*cb)(void *data, int fd),
-        void *data)
-{
-  int open_max;
-  int fd;
-  int res = 0;
-
-  struct rlimit rl;
-
-#ifdef __linux__
-  DIR *d;
-
-  if ((d = opendir ("/proc/self/fd"))) {
-      struct dirent *de;
-
-      while ((de = readdir (d))) {
-          long l;
-          char *e = NULL;
-
-          if (de->d_name[0] == '.')
-              continue;
-
-          errno = 0;
-          l = strtol (de->d_name, &e, 10);
-          if (errno != 0 || !e || *e)
-              continue;
-
-          fd = (int) l;
-
-          if ((long) fd != l)
-              continue;
-
-          if (fd == dirfd (d))
-              continue;
-
-          if ((res = cb (data, fd)) != 0)
-              break;
-        }
-
-      closedir (d);
-      return res;
-  }
-
-  /* If /proc is not mounted or not accessible we fall back to the old
-   * rlimit trick */
-
-#endif
-
-  if (getrlimit (RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
-      open_max = rl.rlim_max;
-  else
-      open_max = sysconf (_SC_OPEN_MAX);
-
-  for (fd = 0; fd < open_max; fd++)
-      if ((res = cb (data, fd)) != 0)
-          break;
-
-  return res;
-}
-
-#endif /* HAVE_FDWALK */
-
 static char *
 read_string (int fd,
              int consume)
@@ -416,8 +331,6 @@ setup_child (const char **args,
              int outp[2],
              int errp[2])
 {
-  int from;
-
   assert (pwd);
   assert (pwd->pw_dir);
 
@@ -430,8 +343,7 @@ setup_child (const char **args,
       exit (EXIT_FAILURE);
     }
 
-  from = STDERR + 1;
-  if (fdwalk (closefd, &from) < 0)
+  if (cockpit_close_range (STDERR + 1, INT_MAX, 0) < 0)
     {
       error ("couldn't close all file descirptors");
       exit (EXIT_FAILURE);

src/ws/cockpitauth.c
@@ -28,6 +28,7 @@
 
 #include "common/cockpitauthorize.h"
 #include "common/cockpitconf.h"
+#include "common/cockpitcloserange.h"
 #include "common/cockpiterror.h"
 #include "common/cockpithex.h"
 #include "common/cockpitjson.h"
@@ -35,7 +36,6 @@
 #include "common/cockpitpipe.h"
 #include "common/cockpitpipetransport.h"
 #include "common/cockpitsystem.h"
-#include "common/cockpitunixfd.h"
 #include "common/cockpitwebserver.h"
 
 #include <security/pam_appl.h>
@@ -507,7 +507,7 @@ session_child_setup (gpointer data)
 
   close (child->io);
 
-  if (cockpit_unix_fd_close_all (3, -1) < 0)
+  if (cockpit_close_range (3, INT_MAX, 0) < 0)
     {
       g_printerr ("couldn't close file descriptors: %m\n");
       _exit (127);

src/ws/session-utils.c
@@ -19,15 +19,14 @@
 
 #include "session-utils.h"
 
+#include "common/cockpitcloserange.h"
 #include "common/cockpitframe.h"
 #include "common/cockpitjsonprint.h"
 
-#include <dirent.h>
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <sys/param.h>
-#include <sys/resource.h>
 #include <time.h>
 #include <utmp.h>
 
@@ -556,92 +555,6 @@ utmp_log (int login,
     }
 }
 
-int
-closefd (void *data,
-         int fd)
-{
-  int *from = data;
-  if (fd >= *from)
-    {
-      while (close (fd) < 0)
-        {
-          if (errno == EAGAIN || errno == EINTR)
-            continue;
-          if (errno == EBADF || errno == EINVAL)
-            break;
-          warnx ("couldn't close fd in bridge process: %m");
-          return -1;
-        }
-    }
-
-  return 0;
-}
-
-#ifndef HAVE_FDWALK
-
-int
-fdwalk (int (*cb)(void *data, int fd),
-        void *data)
-{
-  int open_max;
-  int fd;
-  int res = 0;
-
-  struct rlimit rl;
-
-#ifdef __linux__
-  DIR *d;
-
-  if ((d = opendir ("/proc/self/fd"))) {
-      struct dirent *de;
-
-      while ((de = readdir (d))) {
-          long l;
-          char *e = NULL;
-
-          if (de->d_name[0] == '.')
-              continue;
-
-          errno = 0;
-          l = strtol (de->d_name, &e, 10);
-          if (errno != 0 || !e || *e)
-              continue;
-
-          fd = (int) l;
-
-          if ((long) fd != l)
-              continue;
-
-          if (fd == dirfd (d))
-              continue;
-
-          if ((res = cb (data, fd)) != 0)
-              break;
-        }
-
-      closedir (d);
-      return res;
-  }
-
-  /* If /proc is not mounted or not accessible we fall back to the old
-   * rlimit trick */
-
-#endif
-
-  if (getrlimit (RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
-      open_max = rl.rlim_max;
-  else
-      open_max = sysconf (_SC_OPEN_MAX);
-
-  for (fd = 0; fd < open_max; fd++)
-      if ((res = cb (data, fd)) != 0)
-          break;
-
-  return res;
-}
-
-#endif /* HAVE_FDWALK */
-
 void
 btmp_log (const char *username,
           const char *rhost)
@@ -814,7 +727,7 @@ fd_remap (const int *remap_fds,
         abort_with_message ("dup2(%d, %d) failed: %m", fds[i], i);
 
   /* close everything else */
-  if (fdwalk (closefd, &n_remap_fds) < 0)
+  if (cockpit_close_range (n_remap_fds, INT_MAX, 0) < 0)
     abort_with_message ("couldn't close all file descriptors");
 }
 

commit 570dbdd47e47b61ad89d4cae7f015429e179f816

common: start using closefrom(), keeping our hack
47bc8b93b7604b0831090e17e043286c4a81f1cb introduced our own version of
close_range(), but this turned out to be the wrong API.  closefrom() is
meanwhile available on modern versions of glibc.  It features a
simplified API, with no chance of failure.

Rename cockpitcloserange.c to cockpitclosefrom.c and move the definition
into cockpithacks.h.  Introduce a configure check for closefrom() and
port all users to the closefrom() API.  In case that's not available, we
redirect those calls to our own cockpit_closefrom() (which is now only
conditionally-built).

Since neither the glibc API nor our own code have any chance of failure,
we can drop all the error checking we were doing before.

Incidentally, this should fix warnings we were occasionally seeing from
Valgrind that we were finding and attempting to close its log fd.
Modern Valgrind wraps closefrom() and prevents this from happening.

configure.ac
@@ -45,6 +45,10 @@ AC_USE_SYSTEM_EXTENSIONS
 AC_SYS_LARGEFILE
 AC_PROG_RANLIB
 
+AC_CHECK_FUNCS(
+    closefrom
+)
+
 AM_SILENT_RULES([yes])
 
 AC_MSG_CHECKING([whether to install to prefix only])

src/bridge/bridge.c
@@ -38,7 +38,6 @@
 #include "cockpitwebsocketstream.h"
 
 #include "common/cockpitchannel.h"
-#include "common/cockpitcloserange.h"
 #include "common/cockpitfdpassing.h"
 #include "common/cockpithacks-glib.h"
 #include "common/cockpitjson.h"

src/common/Makefile-common.am
@@ -39,8 +39,7 @@ noinst_LIBRARIES += libcockpit-common.a
 libcockpit_common_a_SOURCES = \
 	src/common/cockpitchannel.c \
 	src/common/cockpitchannel.h \
-	src/common/cockpitcloserange.c \
-	src/common/cockpitcloserange.h \
+	src/common/cockpitclosefrom.c \
 	src/common/cockpitcontrolmessages.c \
 	src/common/cockpitcontrolmessages.h \
 	src/common/cockpiterror.h src/common/cockpiterror.c \

src/common/cockpitclosefrom.c
@@ -19,7 +19,9 @@
 
 #include "config.h"
 
-#include "cockpitcloserange.h"
+#include "cockpithacks.h"
+
+#ifndef HAVE_CLOSEFROM
 
 #include <assert.h>
 #include <dirent.h>
@@ -31,16 +33,13 @@
 #include <unistd.h>
 #include <sys/resource.h>
 
-int
-cockpit_close_range (int from, int max_fd, int flags)
+void
+cockpit_closefrom (int lowfd)
 {
-  /* we want to keep the API compatible to glibc's upcoming close_range(), but don't implement flags or upper bounds */
-  assert (flags == 0);
-  assert (max_fd == INT_MAX);
-
   DIR *d;
 
-  if ((d = opendir ("/proc/self/fd"))) {
+  if ((d = opendir ("/proc/self/fd")))
+    {
       struct dirent *de;
 
       while ((de = readdir (d))) {
@@ -60,26 +59,27 @@ cockpit_close_range (int from, int max_fd, int flags)
               continue;
 
           /* don't bother about error checking; On Linux, EINTR still closes the fd, and with EBADF we already have a closed fd */
-          if (fd >= from)
+          if (fd >= lowfd)
             close (fd);
         }
 
       closedir (d);
-      return 0;
-  }
-
-  /* If /proc is not mounted or not accessible we fall back to the old
-   * rlimit trick */
-  struct rlimit rl;
-  int open_max;
-
-  if (getrlimit (RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
-      open_max = rl.rlim_max;
+    }
   else
-      open_max = sysconf (_SC_OPEN_MAX);
+    {
+      /* If /proc is not mounted or not accessible we fall back to the old
+       * rlimit trick */
+      struct rlimit rl;
+      int open_max;
 
-  for (int fd = from; fd < open_max; fd++)
-    close (fd);
+      if (getrlimit (RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
+          open_max = rl.rlim_max;
+      else
+          open_max = sysconf (_SC_OPEN_MAX);
 
-  return 0;
+      for (int fd = lowfd; fd < open_max; fd++)
+        close (fd);
+    }
 }
+
+#endif

src/common/cockpitcloserange.h
@@ -1,5 +1,0 @@
-#pragma once
-
-/* Added in Linux 5.9: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=278a5fbaed89dacd04e9d
- * will eventually be in glibc: https://sourceware.org/git/?p=glibc.git;a=commit;h=286286283e9bdc */
-int cockpit_close_range (int from, int max_fd, int flags);

src/common/cockpithacks.h
@@ -45,3 +45,8 @@
 #ifndef PACKAGE_VERSION
 #error config.h should be included from the top of every .c file
 #endif
+
+#ifndef HAVE_CLOSEFROM
+#define closefrom(lowfd) cockpit_closefrom(lowfd)
+void cockpit_closefrom (int lowfd);
+#endif

src/common/cockpitpipe.c
@@ -21,8 +21,8 @@
 
 #include "cockpitpipe.h"
 
-#include "cockpitcloserange.h"
 #include "cockpitflow.h"
+#include "cockpithacks.h"
 #include "cockpitunicode.h"
 
 #include <glib-unix.h>
@@ -1407,11 +1407,8 @@ cockpit_pipe_pty (const gchar **argv,
   pid = forkpty (&fd, NULL, NULL, &winsz);
   if (pid == 0)
     {
-      if (cockpit_close_range (3, INT_MAX, 0) < 0)
-        {
-          g_printerr ("couldn't close file descriptors\n");
-          _exit (127);
-        }
+      closefrom (3);
+
       if (directory)
         {
           if (chdir (directory) < 0)

src/pam-ssh-add/Makefile.am
@@ -1,8 +1,8 @@
 noinst_LIBRARIES += libpam_ssh_add.a
 
 libpam_ssh_add_a_SOURCES = \
-	src/common/cockpitcloserange.h \
-	src/common/cockpitcloserange.c \
+	src/common/cockpitclosefrom.c \
+	src/common/cockpithacks.h \
 	src/common/cockpitmemory.h \
 	src/common/cockpitmemory.c \
 	src/pam-ssh-add/pam-ssh-add.c \

src/pam-ssh-add/pam-ssh-add.c
@@ -45,7 +45,7 @@
 
 #include "pam-ssh-add.h"
 
-#include "../common/cockpitcloserange.h"
+#include "../common/cockpithacks.h"
 #include "../common/cockpitmemory.h"
 
 /* programs that can be overwidden in tests */
@@ -343,11 +343,7 @@ setup_child (const char **args,
       exit (EXIT_FAILURE);
     }
 
-  if (cockpit_close_range (STDERR + 1, INT_MAX, 0) < 0)
-    {
-      error ("couldn't close all file descirptors");
-      exit (EXIT_FAILURE);
-    }
+  closefrom (STDERR + 1);
 
   /* Close unnecessary file descriptors */
   close (inp[READ_END]);

src/session/Makefile-session.am
@@ -1,8 +1,8 @@
 libexec_PROGRAMS += cockpit-session
 
 cockpit_session_SOURCES = \
-	src/common/cockpitcloserange.h \
-	src/common/cockpitcloserange.c \
+	src/common/cockpitclosefrom.c \
+	src/common/cockpithacks.h \
 	src/session/client-certificate.h \
 	src/session/client-certificate.c \
 	src/session/session-utils.c \

src/session/session-utils.c
@@ -21,9 +21,9 @@
 
 #include "session-utils.h"
 
-#include "common/cockpitcloserange.h"
 #include "common/cockpitframe.h"
 #include "common/cockpitjsonprint.h"
+#include "common/cockpithacks.h"
 
 #include <fcntl.h>
 #include <stdarg.h>
@@ -537,8 +537,7 @@ fd_remap (const int *remap_fds,
         abort_with_message ("dup2(%d, %d) failed: %m", fds[i], i);
 
   /* close everything else */
-  if (cockpit_close_range (n_remap_fds, INT_MAX, 0) < 0)
-    abort_with_message ("couldn't close all file descriptors");
+  closefrom (n_remap_fds);
 }
 
 int

src/ws/cockpitauth.c
@@ -27,8 +27,8 @@
 
 #include "common/cockpitauthorize.h"
 #include "common/cockpitconf.h"
-#include "common/cockpitcloserange.h"
 #include "common/cockpiterror.h"
+#include "common/cockpithacks.h"
 #include "common/cockpithex.h"
 #include "common/cockpitjson.h"
 #include "common/cockpitmemory.h"
@@ -514,11 +514,7 @@ session_child_setup (gpointer data)
 
   close (child->io);
 
-  if (cockpit_close_range (3, INT_MAX, 0) < 0)
-    {
-      g_printerr ("couldn't close file descriptors: %m\n");
-      _exit (127);
-    }
+  closefrom (3);
 }
 
 static CockpitTransport *