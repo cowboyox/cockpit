cowboyox/cockpit

commit 72911a3edc083ef68ff2c5e37653b8b25b6068f2

containers/ws: Use a builder container to reduce size
This avoids carrying a lot of unnecessary packages such as fonts, Python
libraries, or openldap. It also drops the rpm/dnf programs/databases/logs. This
reduces the container from 343 to 106 MB (of which 40 MB is Python 3, but we
need that for our cockpit-auth-ssh-key script -- this could eventually be
ported to Rust or so?).

Install coreutils-single, which has enough functionality for us and is
much smaller than coreutils.  Explicitly install sscg, as it's only a
recommends, and we like "nice" certificates.

Drop the `dnf update` call. It's pointless now, as we install all the
packages that we need into a fresh --installroot.

Apply some seddery to cockpit-certificate-helper to deal with COCKPIT_GROUP and
missing /etc/machine-id until the proper fixes are in Fedora and our COPR.

During image-prepare, unpack the locally built RPMs on the host, and copy their
files into the container, as we don't have rpm inside the container any more.

containers/ws/install.sh
@@ -13,10 +13,8 @@ package_name()
 
 OSVER=$(. /etc/os-release && echo "$VERSION_ID")
 
-dnf -y update
-
-INSTALL="dnf install -y"
-$INSTALL util-linux-core sed
+INSTALL="dnf install -y --installroot=/build --releasever=$OSVER --setopt=install_weak_deps=False"
+$INSTALL coreutils-single util-linux-core sed sscg python3
 
 arch=`uname -p`
 rpm=$(ls /container/rpms/cockpit-ws-*$OSVER.*$arch.rpm /container/rpms/cockpit-bridge-*$OSVER.*$arch.rpm || true)
@@ -32,5 +30,8 @@ else
     $INSTALL "$ws" "$bridge"
 fi
 
-dnf clean all
+# HACK: fix for older cockpit-certificate-helper
+sed -i '/^COCKPIT_GROUP=/ s/=.*$/=/; s_/etc/machine-id_/dev/null_' /build/usr/libexec/cockpit-certificate-helper
+
+rm -rf /build/var/cache/dnf /build/var/lib/dnf /build/var/lib/rpm* /build/var/log/*
 rm -rf /container/rpms || true

containers/ws/Dockerfile
@@ -1,12 +1,16 @@
-FROM registry.fedoraproject.org/fedora:36
+FROM registry.fedoraproject.org/fedora:36 AS builder
 LABEL maintainer="cockpit-devel@lists.fedorahosted.org"
 LABEL VERSION=main
 
 ARG VERSION
-
 ADD . /container
 
 RUN /container/install.sh
+
+FROM scratch
+COPY --from=builder /build /
+
+ADD . /container
 
 LABEL INSTALL="docker run --rm --privileged -v /:/host -e IMAGE=\${IMAGE} \${IMAGE} /container/label-install \${IMAGE}"
 LABEL UNINSTALL="docker run --rm --privileged -v /:/host -e IMAGE=\${IMAGE} \${IMAGE} /container/label-uninstall"

test/fedora-coreos.install
@@ -8,10 +8,13 @@ rpm-ostree install --cache-only cockpit-bridge-*.rpm \
     cockpit-networkmanager-*.rpm cockpit-system-*.rpm cockpit-tests-*.rpm
 
 # update cockpit-ws and install scripts in the container
+for rpm in /var/tmp/cockpit-ws-*.rpm /var/tmp/cockpit-bridge-*.rpm; do
+    rpm2cpio "$rpm" | cpio -i --make-directories --directory=/var/tmp/install
+done
 podman run --name build-cockpit -i \
     -v /var/tmp/:/run/build:Z \
     quay.io/cockpit/ws sh -exc '
-rpm --freshen --verbose /run/build/cockpit-ws-*.rpm /run/build/cockpit-bridge-*.rpm
+cp -a /run/build/install/* /
 cp /run/build/containers/ws/label-* /run/build/containers/ws/default-bastion.conf /run/build/containers/ws/cockpit-auth-ssh-key /container/
 '
 podman commit --change CMD=/container/label-run build-cockpit localhost/cockpit/ws

commit 81a4da29ee1d16ba674d48552e51347d3bf24eb0

containers/ws: Fix pulling in packages from COPR
Commit 72911a3edc0 broke building the official container against COPR:
`dnf --installroot` only considers the repos in the target tree, not on
the host. So create cockpit.repo in /build.

containers/ws/install.sh
@@ -24,7 +24,7 @@ if [ -n "$rpm" ]; then
     $INSTALL /container/rpms/cockpit-ws-*$OSVER.*$arch.rpm /container/rpms/cockpit-bridge-*$OSVER.*$arch.rpm
 else
     # pull packages from https://copr.fedorainfracloud.org/coprs/g/cockpit/cockpit-preview/
-    echo -e '[group_cockpit-cockpit-preview]\nname=Copr repo for cockpit-preview owned by @cockpit\nbaseurl=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/fedora-$releasever-$basearch/\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/pubkey.gpg\nrepo_gpgcheck=0\nenabled=1\nenabled_metadata=1' > /etc/yum.repos.d/cockpit.repo
+    echo -e '[group_cockpit-cockpit-preview]\nname=Copr repo for cockpit-preview owned by @cockpit\nbaseurl=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/fedora-$releasever-$basearch/\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/pubkey.gpg\nrepo_gpgcheck=0\nenabled=1\nenabled_metadata=1' > /build/etc/yum.repos.d/cockpit.repo
     ws=$(package_name "cockpit-ws")
     bridge=$(package_name "cockpit-bridge")
     $INSTALL "$ws" "$bridge"