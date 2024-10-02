cowboyox/cockpit

commit 97960fa314d6c183b301a41266767ddce6afe363

networkmanager: Port edit network ip settings dialog to React - PF4

pkg/networkmanager/ip-settings.jsx
@@ -1,0 +1,364 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2021 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, { useState, useContext, useEffect } from 'react';
+import cockpit from 'cockpit';
+import {
+    Button,
+    Flex,
+    FormFieldGroup, FormFieldGroupHeader,
+    FormGroup,
+    FormSelect, FormSelectOption,
+    Grid,
+    Switch,
+    TextInput,
+} from '@patternfly/react-core';
+
+import { MinusIcon, PlusIcon } from '@patternfly/react-icons';
+
+import { NetworkModal, dialogApply } from './dialogs-common.jsx';
+import { ModelContext } from './model-context.jsx';
+
+const _ = cockpit.gettext;
+
+export const ipv4_method_choices =
+    [
+        { choice: 'auto', title: _("Automatic (DHCP)") },
+        { choice: 'link-local', title: _("Link local") },
+        { choice: 'manual', title: _("Manual") },
+        { choice: 'shared', title: _("Shared") },
+        { choice: 'disabled', title: _("Disabled") }
+    ];
+
+export const ipv6_method_choices =
+    [
+        { choice: 'auto', title: _("Automatic") },
+        { choice: 'dhcp', title: _("Automatic (DHCP only)") },
+        { choice: 'link-local', title: _("Link local") },
+        { choice: 'manual', title: _("Manual") },
+        { choice: 'ignore', title: _("Ignore") }
+    ];
+
+export const IpSettingsDialog = ({ topic, connection, dev, setIsOpen, settings }) => {
+    const idPrefix = "network-ip-settings";
+    const model = useContext(ModelContext);
+
+    const params = settings[topic];
+    const [addresses, setAddresses] = useState(params.addresses ? params.addresses.map(addr => ({ address: addr[0], netmask: addr[1], gateway: addr[2] })) : []);
+    const [dialogError, setDialogError] = useState(undefined);
+    const [dns, setDns] = useState(params.dns || []);
+    const [dnsSearch, setDnsSearch] = useState(params.dns || []);
+    const [ignoreAutoDns, setIgnoreAutoDns] = useState();
+    const [ignoreAutoRoutes, setIgnoreAutoRoutes] = useState();
+    const [method, setMethod] = useState(params.method);
+    const [routes, setRoutes] = useState(params.routes ? params.routes.map(addr => ({ address: addr[0], netmask: addr[1], gateway: addr[2], metric: addr[3] })) : []);
+
+    // The link local, shared, and disabled methods can't take any
+    // addresses, dns servers, or dns search domains.  Routes,
+    // however, are ok, even for "disabled" and "ignored".  But
+    // since that doesn't make sense, we remove routes as well for
+    // these methods.
+    const isOff = (method == "disabled" || method == "ignore");
+    const canHaveExtra = !(method == "link-local" || method == "shared" || isOff);
+
+    // The auto_*_btns only make sense when the address method
+    // is "auto" or "dhcp".
+    const canAuto = (method == "auto" || method == "dhcp");
+
+    useEffect(() => {
+        // The manual method needs at least one address
+        if (method == 'manual' && addresses.length == 0)
+            setAddresses([{ address: "", netmask: "", gateway: "" }]);
+
+        if (!canHaveExtra) {
+            setAddresses([]);
+            setDns([]);
+            setDnsSearch([]);
+        }
+
+        if (isOff)
+            setRoutes([]);
+    }, [method, addresses.length, canHaveExtra, isOff]);
+
+    const onSubmit = (ev) => {
+        const createSettingsObj = () => ({
+            ...settings,
+            [topic]: {
+                ...settings[topic],
+                method,
+                addresses: addresses.map(addr => [addr.address, addr.netmask, addr.gateway]),
+                dns,
+                dnsSearch,
+                routes: routes.map(route => [route.address, route.netmask, route.gateway, route.metric]),
+                ignore_auto_dns: ignoreAutoDns,
+                ignore_auto_routes: ignoreAutoRoutes,
+            }
+        });
+
+        dialogApply({
+            model,
+            dev,
+            connection,
+            settings: createSettingsObj(),
+            setDialogError,
+            setIsOpen,
+        });
+
+        // Prevent dialog from closing because of <form> onsubmit event
+        if (event)
+            event.preventDefault();
+
+        return false;
+    };
+
+    return (
+        <NetworkModal dialogError={dialogError}
+                      idPrefix={idPrefix}
+                      onSubmit={onSubmit}
+                      setIsOpen={setIsOpen}
+                      title={topic == "ipv4" ? _("IPv4 settings") : _("IPv6 settings")}
+                      isFormHorizontal={false}
+        >
+            <FormFieldGroup
+                data-field='addresses'
+                header={
+                    <FormFieldGroupHeader
+                        titleText={{ text: _("Addresses") }}
+                        actions={
+                            <Flex>
+                                <FormSelect className="network-ip-settings-method"
+                                            id={idPrefix + "-select-method"}
+                                            aria-label={_("Select method")}
+                                            onChange={setMethod}
+                                            value={method}>
+                                    {(topic == "ipv4" ? ipv4_method_choices : ipv6_method_choices).map(choice => <FormSelectOption value={choice.choice} label={choice.title} key={choice.choice} />)}
+                                </FormSelect>
+                                <Button variant="secondary"
+                                        isDisabled={!canHaveExtra}
+                                        onClick={() => setAddresses([...addresses, { address: "", netmask: "", gateway: "" }])}
+                                        id={idPrefix + "-address-add"}
+                                        aria-label={_("Add item")}
+                                        icon={<PlusIcon />} />
+                            </Flex>
+                        }
+                    />
+                }
+            >
+                {addresses.map((address, i) => {
+                    const prefixText = (topic == "ipv4") ? _("Prefix length or netmask") : _("Prefix length");
+
+                    return (
+                        <Grid key={i} hasGutter>
+                            <FormGroup fieldId={idPrefix + "-address-" + i} label={_("Address")} className="pf-m-4-col-on-sm">
+                                <TextInput id={idPrefix + "-address-" + i} value={address.address} onChange={value => setAddresses(
+                                    addresses.map((item, index) =>
+                                        i === index
+                                            ? { ...item, address: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup fieldId={idPrefix + "-netmask-" + i} label={prefixText} className="pf-m-4-col-on-sm">
+                                <TextInput id={idPrefix + "-netmask-" + i} value={address.netmask} onChange={value => setAddresses(
+                                    addresses.map((item, index) =>
+                                        i === index
+                                            ? { ...item, netmask: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup fieldId={idPrefix + "-gateway-" + i} label={_("Gateway")} className="pf-m-4-col-on-sm">
+                                <TextInput id={idPrefix + "-gateway-" + i} value={address.gateway} onChange={value => setAddresses(
+                                    addresses.map((item, index) =>
+                                        i === index
+                                            ? { ...item, gateway: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup className="pf-m-1-col-on-sm remove-button-group">
+                                <Button variant='secondary'
+                                        isDisabled={method == 'manual' && i == 0}
+                                        onClick={() => setAddresses(addresses.filter((_, index) => index !== i))}
+                                        aria-label={_("Remove item")}
+                                        icon={<MinusIcon />} />
+                            </FormGroup>
+                        </Grid>
+                    );
+                })}
+            </FormFieldGroup>
+            <FormFieldGroup
+                data-field='dns'
+                header={
+                    <FormFieldGroupHeader
+                        titleText={{ text: _("DNS") }}
+                        actions={
+                            <Flex alignItems={{ default: 'alignItemsCenter' }}>
+                                <Switch
+                                    isChecked={!ignoreAutoDns}
+                                    isDisabled={!canAuto}
+                                    onChange={value => setIgnoreAutoDns(!value)}
+                                    label={_("Automatic")} />
+                                <Button variant="secondary"
+                                        isDisabled={!canHaveExtra}
+                                        onClick={() => setDns([...dns, ""])}
+                                        id={idPrefix + "-dns-add"}
+                                        aria-label={_("Add item")}
+                                        icon={<PlusIcon />} />
+                            </Flex>
+                        }
+                    />
+                }
+            >
+                {dns.map((server, i) => {
+                    return (
+                        <Grid key={i} hasGutter>
+                            <FormGroup fieldId={idPrefix + "-dns-server-" + i} label={_("Server")}>
+                                <TextInput id={idPrefix + "-dns-server-" + i} value={server} onChange={value => setDns(
+                                    dns.map((item, index) =>
+                                        i === index
+                                            ? value
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup className="pf-m-1-col-on-sm remove-button-group">
+                                <Button variant='secondary'
+                                        isSmall
+                                        onClick={() => setDns(dns.filter((_, index) => index !== i))}
+                                        aria-label={_("Remove item")}
+                                        icon={<MinusIcon />} />
+                            </FormGroup>
+                        </Grid>
+                    );
+                })}
+            </FormFieldGroup>
+            <FormFieldGroup
+                data-field='dns_search'
+                header={
+                    <FormFieldGroupHeader
+                        titleText={{ text: _("DNS search domains") }}
+                        actions={
+                            <Flex alignItems={{ default: 'alignItemsCenter' }}>
+                                <Switch
+                                    isChecked={!ignoreAutoDns}
+                                    isDisabled={!canAuto}
+                                    onChange={value => setIgnoreAutoDns(!value)}
+                                    label={_("Automatic")} />
+                                <Button variant="secondary"
+                                        isDisabled={!canHaveExtra}
+                                        onClick={() => setDnsSearch([...dnsSearch, ""])}
+                                        id={idPrefix + "-dnf-search-add"}
+                                        aria-label={_("Add item")}
+                                        icon={<PlusIcon />} />
+                            </Flex>
+                        }
+                    />
+                }
+            >
+                {dnsSearch.map((domain, i) => {
+                    return (
+                        <Grid key={i} hasGutter>
+                            <FormGroup fieldId={idPrefix + "-search-domain"} label={_("Search domain")}>
+                                <TextInput id={idPrefix + "-search-domain"} value={domain} onChange={value => setDnsSearch(
+                                    dnsSearch.map((item, index) =>
+                                        i === index
+                                            ? value
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup className="pf-m-1-col-on-sm remove-button-group">
+                                <Button variant='secondary'
+                                        isSmall
+                                        onClick={() => setDnsSearch(dnsSearch.filter((_, index) => index !== i))}
+                                        aria-label={_("Remove item")}
+                                        icon={<MinusIcon />} />
+                            </FormGroup>
+                        </Grid>
+                    );
+                })}
+            </FormFieldGroup>
+            <FormFieldGroup
+                data-field='routes'
+                header={
+                    <FormFieldGroupHeader
+                        titleText={{ text: _("Routes") }}
+                        actions={
+                            <Flex alignItems={{ default: 'alignItemsCenter' }}>
+                                <Switch
+                                    isChecked={!ignoreAutoRoutes}
+                                    isDisabled={!canAuto}
+                                    onChange={value => setIgnoreAutoRoutes(!value)}
+                                    label={_("Automatic")} />
+                                <Button variant="secondary"
+                                        isDisabled={isOff}
+                                        onClick={() => setRoutes([...routes, { address: "", netmask: "", gateway: "", metric: "" }])}
+                                        id={idPrefix + "-route-add"}
+                                        aria-label={_("Add item")}
+                                        icon={<PlusIcon />} />
+                            </Flex>
+                        }
+                    />
+                }
+            >
+                {routes.map((route, i) => {
+                    return (
+                        <Grid key={i} hasGutter>
+                            <FormGroup fieldId={idPrefix + "-route-address-" + i} label={_("Address")} className="pf-m-3-col-on-sm">
+                                <TextInput id={idPrefix + "-route-address-" + i} value={route.address} onChange={value => setRoutes(
+                                    routes.map((item, index) =>
+                                        i === index
+                                            ? { ...item, address: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup fieldId={idPrefix + "-route-netmask-" + i} label={_("Prefix length or netmask")} className="pf-m-4-col-on-sm">
+                                <TextInput id={idPrefix + "-route-netmask-" + i} value={route.netmask} onChange={value => setRoutes(
+                                    routes.map((item, index) =>
+                                        i === index
+                                            ? { ...item, netmask: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup fieldId={idPrefix + "-route-gateway-" + i} label={_("Gateway")} className="pf-m-3-col-on-sm">
+                                <TextInput id={idPrefix + "-route-gateway-" + i} value={route.gateway} onChange={value => setRoutes(
+                                    routes.map((item, index) =>
+                                        i === index
+                                            ? { ...item, gateway: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup fieldId={idPrefix + "-route-metric-" + i} label={_("Metric")} className="pf-m-2-col-on-sm">
+                                <TextInput id={idPrefix + "-route-metric-" + i} value={route.metric} onChange={value => setRoutes(
+                                    routes.map((item, index) =>
+                                        i === index
+                                            ? { ...item, metric: value }
+                                            : item
+                                    ))} />
+                            </FormGroup>
+                            <FormGroup className="pf-m-1-col-on-sm remove-button-group">
+                                <Button variant='secondary'
+                                        isSmall
+                                        onClick={() => setRoutes(routes.filter((_, index) => index !== i))}
+                                        aria-label={_("Remove item")}
+                                        icon={<MinusIcon />} />
+                            </FormGroup>
+                        </Grid>
+                    );
+                })}
+            </FormFieldGroup>
+        </NetworkModal>
+    );
+};

pkg/networkmanager/dialogs-common.jsx
@@ -29,6 +29,7 @@ import {
 import { BondDialog, getGhostSettings as getBondGhostSettings } from './bond.jsx';
 import { BridgeDialog, getGhostSettings as getBridgeGhostSettings } from './bridge.jsx';
 import { BridgePortDialog } from './bridgeport.jsx';
+import { IpSettingsDialog } from './ip-settings.jsx';
 import { TeamDialog, getGhostSettings as getTeamGhostSettings } from './team.jsx';
 import { TeamPortDialog } from './teamport.jsx';
 import { VlanDialog, getGhostSettings as getVlanGhostSettings } from './vlan.jsx';
@@ -135,7 +136,7 @@ export const Name = ({ idPrefix, iface, setIface }) => {
     );
 };
 
-export const NetworkModal = ({ dialogError, help, idPrefix, setIsOpen, title, onSubmit, children }) => {
+export const NetworkModal = ({ dialogError, help, idPrefix, setIsOpen, title, onSubmit, children, isFormHorizontal }) => {
     return (
         <Modal id={idPrefix + "-dialog"} position="top" variant="medium"
             isOpen
@@ -153,7 +154,7 @@ export const NetworkModal = ({ dialogError, help, idPrefix, setIsOpen, title, on
                 </>
             }
         >
-            <Form id={idPrefix + "-body"} onSubmit={onSubmit} isHorizontal>
+            <Form id={idPrefix + "-body"} onSubmit={onSubmit} isHorizontal={isFormHorizontal !== false}>
                 {dialogError && <ModalError id={idPrefix + "-error"} dialogError={_("Failed to apply settings")} dialogErrorDetail={dialogError} />}
                 {children}
             </Form>
@@ -229,6 +230,8 @@ export const NetworkAction = ({ buttonText, iface, connectionSettings, type }) =
             {isOpen && type == 'mac' ? <MacDialog {...properties} /> : null}
             {isOpen && type == 'teamport' ? <TeamPortDialog {...properties} /> : null}
             {isOpen && type == 'bridgeport' ? <BridgePortDialog {...properties} /> : null}
+            {isOpen && type == 'ipv4' ? <IpSettingsDialog topic="ipv4" {...properties} /> : null}
+            {isOpen && type == 'ipv6' ? <IpSettingsDialog topic="ipv6" {...properties} /> : null}
         </>
     );
 };

pkg/networkmanager/index.html
@@ -40,35 +40,6 @@
     <h1 id="testing-connection-curtain-restoring" translate="yes">Restoring connection</h1>
   </div>
 
-  <div hidden aria-hidden="true" id="network-ip-settings-dialog">
-    <div class="pf-c-backdrop">
-      <div class="pf-l-bullseye">
-        <div class="pf-c-modal-box pf-m-md pf-m-align-top" role="dialog" aria-modal="true">
-          <button class="pf-c-button pf-m-plain" type="button" aria-label="Close" id="network-ip-settings-close-button" translate="aria-label">
-            <i class="fa fa-times" aria-hidden="true"></i>
-          </button>
-          <header class="pf-c-modal-box__header">
-            <h1 class="pf-c-modal-box__title" translate="yes">IP settings</h1>
-          </header>
-          <div class="pf-c-modal-box__body">
-            <div id="network-ip-settings-body">
-            </div>
-          </div>
-          <footer class="pf-c-modal-box__footer">
-            <div class="pf-c-alert pf-m-danger pf-m-inline dialog-error" aria-label="inline danger alert" id="network-ip-settings-error">
-              <div class="pf-c-alert__icon">
-                <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-              </div>
-              <h4 class="pf-c-alert__title"></h4>
-            </div>
-            <button class="pf-c-button pf-m-primary" translate="yes" id="network-ip-settings-apply">Apply</button>
-            <button class="pf-c-button pf-m-link" translate="yes" id="network-ip-settings-cancel">Cancel</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div hidden aria-hidden="true" id="error-popup">
     <div class="pf-c-backdrop">
       <div class="pf-l-bullseye">

commit cf6df8a1ef6d867ce3f8b9295ebe2c4096e9e77c

networkmanager: remove incorrect preventDefault invocation
This was introduced in 97960fa314, where the IpSettingsDialog got ported
to React and PF4. But never called `preventDefault` for the onClick
event and neither is it part of the form.

pkg/networkmanager/ip-settings.jsx
@@ -108,7 +108,7 @@ export const IpSettingsDialog = ({ topic, connection, dev, settings }) => {
             setRoutes([]);
     }, [method, addresses.length, canHaveExtra, isOff]);
 
-    const onSubmit = (ev) => {
+    const onSubmit = (_ev) => {
         const createSettingsObj = () => ({
             ...settings,
             [topic]: {
@@ -131,12 +131,6 @@ export const IpSettingsDialog = ({ topic, connection, dev, settings }) => {
             setDialogError,
             onClose: Dialogs.close,
         });
-
-        // Prevent dialog from closing because of <form> onsubmit event
-        if (event)
-            event.preventDefault();
-
-        return false;
     };
     const addressIpv4Helper = (address) => {
         const config = { address, netmask: '', gateway: '' };